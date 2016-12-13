42aa1dda81d122289086e1607a1ed2b47c9a7e5a
Enrico Sacchetti <esacchetti@gmail.com>
2016-12-13 06:24:58 -0500

    add dynamic participant rows

--- site/js/components/participant.js
+++ site/js/components/participant.js
@@ -3,6 +3,13 @@ Vue.component('participant', {
     'index',
     'item'
   ],
+
+  data: function () {
+    return {
+      nextRowAdded: false
+    }
+  },
+
   computed: {
     num: function () {
       return this.index + 1
@@ -11,19 +18,27 @@ Vue.component('participant', {
 
   methods: {
     addRow: function () {
+      if (this.nextRowAdded) return
+      this.$parent.participantList.push({
+        name: '',
+        email: ''
+      })
 
+      // Reset material lite components
+      componentHandler.upgradeDom()
+      this.nextRowAdded = true
     }
   },
 
-  template: `
-    <div class="participant">
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label notes-head_people">
-        <input class="mdl-textfield__input" type="text" id="sample1" :value="item.name">
-        <label class="mdl-textfield__label" for="sample1">Participant {{num}} Name</label>
-      </div>
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label notes-head_people">
-        <input class="mdl-textfield__input" type="email" id="sample1" :value="item.email">
-        <label class="mdl-textfield__label" for="sample1">Email</label>
-      </div>
-    </div>`
+  template: '\
+    <div class="participant">\
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label notes-head_people">\
+        <input class="mdl-textfield__input" type="text" id="sample1" :value="item.name" v-on:keydown="addRow">\
+        <label class="mdl-textfield__label" for="sample1">Participant {{num}} Name</label>\
+      </div>\
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label notes-head_people">\
+        <input class="mdl-textfield__input" type="email" id="sample1" :value="item.email" v-on:keydown="addRow">\
+        <label class="mdl-textfield__label" for="sample1">Email</label>\
+      </div>\
+    </div>'
 })
